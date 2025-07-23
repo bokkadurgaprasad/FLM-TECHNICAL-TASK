dd08c6e9f7fe14d20563a330066fd53e498811be
durgaprasadbokka <durgaprasadbokka21@gmail.com>
2025-07-24 00:58:39 +0530

    added comments

--- email_reader.py
+++ email_reader.py
@@ -1,12 +1,15 @@
 import re
+#re for pattern matching
 
 def filter_email_subjects(subjects, keyword):
+    '''Filters a list of email subjects based on a keyword'''
     filtered_subjects = []
     for subject in subjects:
         if re.search(keyword.lower(), subject.lower()):
             filtered_subjects.append(subject)
     return filtered_subjects
 
+#sample data
 sample_subjects = ["New Lead from Form", "Daily Report", "Zoom Invite", "Cybersecurity Lead"]
 filtered = filter_email_subjects(sample_subjects, "Lead")
 for subject in filtered:
